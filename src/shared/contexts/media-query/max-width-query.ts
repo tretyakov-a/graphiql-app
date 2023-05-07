9b01f8cefaee235f7c0beb28615694f62c041df9
Alexander Tretyakov <tpetyakov@gmail.com>
2023-05-07 20:45:47 +0300

    feat: add media query context

---
+++ src/shared/contexts/media-query/max-width-query.ts
@@ -0,0 +1,10 @@
+type BreakpointKeys = 'xs' | 'sm' | 'md' | 'lg';
+const breakpoints: Record<BreakpointKeys, number> = {
+  xs: 576,
+  sm: 768,
+  md: 1024,
+  lg: 1200,
+};
+
+export const maxWidthQuery = (breakpointKey: BreakpointKeys) =>
+  `(max-width: ${breakpoints[breakpointKey]}px)`;
