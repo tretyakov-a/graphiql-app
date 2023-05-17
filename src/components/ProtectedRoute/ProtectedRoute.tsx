bf55d6f4c76249539d1297f78e50882780b98044
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-05-17 20:40:59 +0400

    refactor: change Protect Route component

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,21 +1,16 @@
-import { User } from 'firebase/auth';
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 interface ProtectedRouteProps extends React.PropsWithChildren {
-  user: User | null | undefined;
-  isNeedable: boolean;
+  passCondition: boolean;
   route: string;
 }
-
 export const ProtectedRoute = (props: ProtectedRouteProps) => {
-  const { user, children, isNeedable, route } = props;
+  const { passCondition, children, route } = props;
 
-  if (!user && isNeedable) {
-    return <Navigate to={route} replace />;
-  } else if (user && !isNeedable) {
+  if (!passCondition) {
     return <Navigate to={route} replace />;
   }
 
-  return children as unknown as JSX.Element;
+  return <>{children}</>;
 };
