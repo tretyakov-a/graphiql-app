16d8471fa60dfec00a385a10071d313b375201d8
Alexander Tretyakov <tpetyakov@gmail.com>
2023-05-18 17:08:17 +0300

    refactor: add HeadeMenu

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,34 +1,16 @@
 import classes from './style.module.scss';
 import generalClasses from '@src/styles/general.module.scss';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { useTranslation } from 'react-i18next';
-import { NavLink } from 'react-router-dom';
 import { auth, logout } from '@src/shared/api/firebase';
 import LanguageSelector from '@src/components/LanguageSelector';
-import Loader from '@src/components/Loader';
 import IconButton from '@src/components/IconButton';
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
-import { LinkInfo, authorizedLinks, unauthorizedLinks } from './links';
 import Logo from './Logo';
 import { classNames } from '@src/shared/utils';
+import HeaderMenu from './HeaderMenu';
 
 const Header = () => {
-  const { t } = useTranslation();
-  const [user, loading] = useAuthState(auth);
-
-  const renderLinks = (links: LinkInfo[]) =>
-    links.map(({ to, langKey }) => (
-      <li key={to}>
-        <NavLink
-          to={to}
-          className={({ isActive }) =>
-            classNames([classes.menuLink, isActive && classes.menuLinkActive])
-          }
-        >
-          {t(langKey)}
-        </NavLink>
-      </li>
-    ));
+  const [user] = useAuthState(auth);
 
   const isAuthorized = Boolean(user);
 
@@ -37,19 +19,17 @@ const Header = () => {
       <div className={classNames([generalClasses.container, classes.headerContainer])}>
         <Logo />
         <div className={classes.headerRight}>
-          <nav className={classes.menuContainer}>
-            {loading ? (
-              <Loader size={'sm'} />
-            ) : (
-              <ul className={classes.menu}>
-                {isAuthorized ? renderLinks(authorizedLinks) : renderLinks(unauthorizedLinks)}
-              </ul>
-            )}
+          <nav className={classes.headerMenuContainer}>
+            <HeaderMenu />
           </nav>
           <div className={classes.toolbar}>
             {isAuthorized && (
               <div>
-                <IconButton onClick={logout} icon={faRightFromBracket} />
+                <IconButton
+                  onClick={logout}
+                  tooltip={{ langKey: 'logout' }}
+                  icon={faRightFromBracket}
+                />
               </div>
             )}
             <LanguageSelector />
